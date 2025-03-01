163f413677c9935df9e095d5bdb192915cb268c1
hanif-alkahfy <hanifalkahfy@gmail.com>
2025-03-01 14:11:16 +0700

    Fixed Footer dan Navbar di frontend

--- findyourclass-frontend/src/components/Navbar.jsx
+++ findyourclass-frontend/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import ReactLogo from "../assets/react.svg";
+import AmikomLogo from "../assets/amikom.svg";
 
 const Navbar = () => {
   return (
     <nav className="bg-white py-8 w-full fixed top-0 left-0 shadow-md">
       <div className="container mx-auto flex items-center px-4">
-        <img src={ReactLogo} alt="Logo" className="ml-5" />
+        <img src={AmikomLogo} alt="Logo" className="ml-5 w-10" />
       </div>
     </nav>
   );
